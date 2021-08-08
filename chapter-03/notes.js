33d1b4d62d8e967c40b22cec8209a3121e0d2621
Ashley Toney <amtoney524@gmail.com>
2021-08-08 06:47:03 -0500

    Added notes on function optionl args, default params, closures, and recursion

--- chapter-03/notes.js
+++ chapter-03/notes.js
@@ -83,3 +83,100 @@ function future() {
 //function square(num) { return num * num }
 const square = (num) => { return num * num};
 console.log(square(7), "\n");
+
+/*
+  Optional Arguments:
+    - if too many arguments passed to a function, the extra arguments are ignored
+    - if too few arguments passed, the missing parameters are assigned a value of undefined
+*/
+
+console.log(square(2, "cat")); // returns 4
+console.log(square(), "\n"); // returns undefined * undefined, or NaN
+
+/* 
+  Default Parameters:
+  If you write an = operator after a parameter, followed by an expression,
+  the value of that expression will replace the argument when it is not given
+*/
+
+function power(base, exponent=2) {
+  let result = 1;
+  
+  for (let i = 0; i < exponent; i++) {
+    result *= base;
+  }
+
+  return result;
+}
+
+console.log(power(2, 3)); // returns 8
+console.log(power(2), "\n"); // returns 4, since missing parameter is assigned default
+
+/*
+  Closures:
+  Every time a function is called, local bindings are created
+  These local bindings may be accessed and returned as closures
+
+  Closure = the ability to reference a local binding in the enclosing scope
+*/
+
+function wrapValue(n) {
+  let local = n;
+  return () => local; // returns function which access & returns local binding "local"
+}
+
+let wrap1 = wrapValue(1);
+let wrap2 = wrapValue(2);
+
+console.log(wrap1(), wrap2(), "\n"); // returns 1 2
+
+// Use closures to create functions that multiply by an arbitrary amount
+function multiplier (factor) {
+  return (number) => number * factor;
+}
+
+let twice = multiplier(2);
+let threeTimes = multiplier(3);
+
+console.log(twice(10)); // returns 20
+console.log(threeTimes(10), "\n"); // returns 30
+
+/* 
+  Recursion: function that calls itself
+  However, in JS, loops are generally less expensive than recursion
+  This creates a tradeoff of speed vs. elegance
+  
+  Yet, some problems are better solved with recursion - like exploration of Trees
+*/
+
+function recursivePowers(base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  } else {
+    return base * recursivePowers(base, exponent - 1);
+  }
+}
+
+console.log(recursivePowers(10, 3), "\n"); // returns 1000
+
+/*
+  Starting from 1 and repeatedly adding 5 or multiplying by 3, infinite numbers can be produced
+  Find a sequence of additions & multiplications that find a certain number
+ */
+
+function findSolution(target) {
+  function find(current, history) {
+    if (current === target) {
+      return history;
+    } else if (current > target) {
+      return null; // allows us to break out of current search
+    } else {
+      return find(current + 5, `(${history} + 5)`) ||
+        find(current * 3, `(${history} * 3)`);
+    }
+  }
+
+  return find(1, "1");
+}
+
+console.log(findSolution(24), "\n");
